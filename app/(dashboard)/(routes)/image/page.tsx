a91df1b779979d77718efb8829f72b9fa5e7fe42
SonuAkhtar <sonua981@gmail.com>
2023-08-11 15:20:19 +0530

    working on image generator

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,15 +1,181 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+
+import * as z from "zod";
 import Heading from "@/components/Heading/Heading";
+import { useForm } from "react-hook-form";
+import { amountOptions, formSchema, resolutionOptions } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/Shadcn/button";
+import { useRouter } from "next/navigation";
+import {
+  SelectTrigger,
+  Select,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/Shadcn/card";
 
 const ImagePage = () => {
+  const router = useRouter();
+  const [images, setImages] = useState<string[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount: "1",
+      resolution: "512*512",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]);
+      const response = await axios.post("/api/image", values);
+
+      const urls = response.data.map((image: { url: string }) => image.url);
+      setImages(urls);
+
+      form.reset();
+    } catch (error: any) {
+      // todo: open pro model
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
   return (
     <div>
       <Heading
         title="Image Generator"
-        description="Most advanced conversation tool"
+        description="Turn your imagination into images"
         icon="fa-solid fa-image"
         iconColor="text-pink-500"
         bgColor="bg-pink-500/10"
       />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-6">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="A picture of a horse in ocean"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {amountOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.value}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="resolution"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {resolutionOptions.map((resolution) => (
+                          <SelectItem
+                            key={resolution.value}
+                            value={resolution.value}
+                          >
+                            {resolution.value}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {images.map((image) => (
+              <Card key={image} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image alt="image" fill src={image} />
+                </div>
+                <CardFooter className="p-2">
+                  <Button
+                    onClick={() => window.open(image)}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -12,24 +12,27 @@ export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { messages } = body;
+    const { prompt, amount = 1, resolution = "512*512" } = body;
 
     if (!userId) return new NextResponse("Not Authorized", { status: 401 });
 
     if (!configuration.apiKey)
       return new NextResponse("API Key not found", { status: 500 });
 
-    if (!messages)
-      return new NextResponse("Messages are required", { status: 400 });
+    if (!prompt) return new NextResponse("Prompt is required", { status: 400 });
+    if (!amount) return new NextResponse("Amount is required", { status: 400 });
+    if (!resolution)
+      return new NextResponse("Resolution is required", { status: 400 });
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages,
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
     });
 
-    return NextResponse.json(response.data.choices[0].message);
+    return NextResponse.json(response.data.data);
   } catch (error) {
-    console.log("Conversation_Error", error);
+    console.log("Image_Error", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
