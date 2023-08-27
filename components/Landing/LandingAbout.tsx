8832ed2d36b07204c2b5c74b50551100e59e8708
SonuAkhtar <sonua981@gmail.com>
2023-08-27 17:20:03 +0530

    landingAbout complete

--- components/Landing/LandingAbout.tsx
+++ components/Landing/LandingAbout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import AboutCard from "../AboutCard/AboutCard";
+import Image from "next/image";
+import { Button } from "@/components/Shadcn/button";
+import "./landingAbout.css";
 
 const aboutDetails = [
   {
@@ -42,7 +44,34 @@ export const LandingAbout = () => {
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-4">
           {aboutDetails.map((item, i) => (
-            <AboutCard item={item} key={i} />
+            <div className="relative w-full px-2 p-2 flex items-center justify-center text-white">
+              <div className="image_wrapper relative overflow-hidden rounded-2xl h-80 w-60 border border-slate-300">
+                <Image
+                  fill
+                  alt="card-image"
+                  className="origin-left transition-all duration-1000"
+                  src={item.image}
+                />
+
+                <div className="card_body text-[#f6f5fa] absolute top-0 left-0 h-full w-full transition-all duration-1000">
+                  <div className="number text-4xl font-thin absolute left-4 transition-all duration-1000">
+                    {item.number}
+                  </div>
+                  <div className="name text-2xl absolute transition-all duration-1000">
+                    {item.name}
+                  </div>
+                  <div className="profile text-xs font-light absolute transition-all duration-1000">
+                    {item.profile}
+                  </div>
+                  <Button
+                    variant="outline"
+                    className="rounded-full know_btn text-black absolute left-4 transition-all duration-1000"
+                  >
+                    Know More
+                  </Button>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
